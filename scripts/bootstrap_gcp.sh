bea1875446bcd1b4825ac32c5dabd4cdc4b08623
Daniel Recuero Cordobés <danielrecuerocord@gmail.com>
2026-04-19 16:56:54 +0200

    feat: first GCP service: GCS bucket for trained information 14th  attempt

--- scripts/bootstrap_gcp.sh
+++ scripts/bootstrap_gcp.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Checking if the bucket '$BUCKET_NAME_TERRAFORM' already exists..."
 REGION="europe-west1"
 BUCKET_BACKEND="${TF_BACKEND_BUCKET}"
-BUCKET_MODELS="${TF_VAR_models_bucket}"
 
-for BUCKET in "$BUCKET_BACKEND" "$BUCKET_MODELS"
-do
-  echo "Checking if the bucket '$BUCKET' already exists..."
-  if gsutil ls -b "gs://$BUCKET" >/dev/null 2>&1; then
-    echo "The GCS bucket '$BUCKET' already exists."
-  else
-    echo "Creating GCS bucket '$BUCKET' in region '$REGION'..."
-    gsutil mb -l "$REGION" "gs://$BUCKET"
-    echo "Enabling versioning..."
-    gsutil versioning set on "gs://$BUCKET"
-    echo "Bucket '$BUCKET' successfully created with versioning enabled."
-  fi
-done
+echo "Checking if the backend bucket '$BUCKET_BACKEND' already exists..."
+if gsutil ls -b "gs://$BUCKET_BACKEND" >/dev/null 2>&1; then
+  echo "The GCS backend bucket '$BUCKET_BACKEND' already exists."
+else
+  echo "Creating GCS backend bucket '$BUCKET_BACKEND' in region '$REGION'..."
+  gsutil mb -l "$REGION" "gs://$BUCKET_BACKEND"
+  echo "Enabling versioning..."
+  gsutil versioning set on "gs://$BUCKET_BACKEND"
+  echo "Backend bucket '$BUCKET_BACKEND' successfully created with versioning enabled."
+fi
