83c6b67637673648048917d848dd426e1cc51761
Pedro Gallego López <pedrogallegolop@gmail.com>
2026-05-01 13:27:02 +0200

    chore: optimize imports

--- src/config/volatility_models_config/volatility_models_config.py
+++ src/config/volatility_models_config/volatility_models_config.py
@@ -1,4 +1,3 @@
-import json
 from pathlib import Path
 
 from src.config.volatility_models_config.training_data_config import TrainingDataConfig
@@ -6,7 +5,4 @@ from src.config.volatility_models_config.training_data_config import TrainingDat
 
 class VolatilityModelsConfig:
     def __init__(self, volatility_models_config_file_path: Path):
-        with open(volatility_models_config_file_path, "r", encoding="utf-8") as file:
-            payload = json.load(file)
-
         self.training_data_config = TrainingDataConfig(volatility_models_config_file_path)
