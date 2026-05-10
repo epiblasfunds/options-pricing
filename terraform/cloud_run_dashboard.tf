b23d822e421a129eae01be3499319d70b41d7965
Daniel Recuero Cordobés <danielrecuerocord@gmail.com>
2026-05-10 10:50:48 +0200

    chore: move startup_cpu_boost to metadata annotations in dashboard service

--- terraform/cloud_run_dashboard.tf
+++ terraform/cloud_run_dashboard.tf
@@ -4,7 +4,11 @@ resource "google_cloud_run_service" "dashboard" {
   location = var.gcp_region
 
   template {
-    startup_cpu_boost = true 
+    metadata {
+      annotations = {
+        "run.googleapis.com/startup-cpu-boost" = "true"
+      }
+    }
     spec {
       service_account_name = var.github_actions_service_account_email
 
